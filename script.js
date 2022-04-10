03f829277ab58a6a6922432fb95faaac235e1609
Pedro Lucas <97309169+1PedroLucas@users.noreply.github.com>
2022-04-10 02:32:08 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+function gerarQRCode(){
+    var inputUsuario = document.querySelector('textarea').value;
+    var GoogleChartAPI = 'https://chart.googleapis.com/chart?cht=qr&chs=250x250&chld=Q&chl=';
+    var conteudoQRCode = GoogleChartAPI +encodeURIComponent(inputUsuario);
+    document.querySelector('#QRCodeImage').src = conteudoQRCode;
+}   
